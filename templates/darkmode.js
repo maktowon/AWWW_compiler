f0ac2d2522ef9c68a32276ac11bf765af8b1b317
aktowon <maciek.nowotek@gmail.com>
2023-03-15 01:59:54 +0100

    tree structure display in the file section

--- templates/darkmode.js
+++ templates/darkmode.js
@@ -1,9 +1,9 @@
-const toggle = document.getElementById('toggle_mode')
+const darkmode = document.getElementById('toggle_dark_mode');
 const root = document.documentElement;
 
-toggle.addEventListener('click', function() {
+darkmode.addEventListener('click', function() {
     this.classList.toggle('bi-brightness-high-fill');
-    if(this.classList.toggle('bi-moon-stars-fill')) {
+    if (this.classList.toggle('bi-moon-stars-fill')) {
         root.style.setProperty('--bg', 'beige');
         root.style.setProperty('--color', 'black');
         root.style.setProperty('--hover_text', 'slateblue');
