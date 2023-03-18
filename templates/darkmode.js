a3f7db8864b1cbb295d131b1a3cd6c23b063e1b4
aktowon <maciek.nowotek@gmail.com>
2023-03-18 04:00:55 +0100

    Add tabs, task completed

--- templates/darkmode.js
+++ templates/darkmode.js
@@ -4,12 +4,18 @@ const root = document.documentElement;
 darkmode.addEventListener('click', function() {
     this.classList.toggle('bi-brightness-high-fill');
     if (this.classList.toggle('bi-moon-stars-fill')) {
-        root.style.setProperty('--bg', 'beige');
+        root.style.setProperty('--color1', 'gray');
+        root.style.setProperty('--color2', 'beige');
+        root.style.setProperty('--color3', 'white');
         root.style.setProperty('--color', 'black');
-        root.style.setProperty('--hover_text', 'slateblue');
+        root.style.setProperty('--folder_fill', 'url(folder-fill.svg)')
+        root.style.setProperty('--file_fill', 'url(file-code-fill.svg)')
     } else {
-        root.style.setProperty('--bg', 'black');
+        root.style.setProperty('--color1', 'var(--color1_dm)');
+        root.style.setProperty('--color2', 'var(--color2_dm)');
+        root.style.setProperty('--color3', 'var(--color3_dm)');
         root.style.setProperty('--color', 'white');
-        root.style.setProperty('--hover_text', 'slategray');
+        root.style.setProperty('--folder_fill', 'url(folder-fill-darkmode.svg)')
+        root.style.setProperty('--file_fill', 'url(file-code-fill-darkmode.svg)')
     }
 })
--- templates/folder.js
+++ templates/folder.js
@@ -2,9 +2,9 @@ const toggler = document.getElementsByClassName('folder');
 
 for (let i = 0; i < toggler.length; i++) {
     toggler[i].addEventListener('click', function () {
-        const cl = this.getElementsByClassName('bi')[0].classList;
-        cl.toggle('bi-folder-fill');
-        cl.toggle('bi-folder');
+        // const cl = this.getElementsByClassName('bi')[0].classList;
+        // cl.toggle('bi-folder-fill');
+        // cl.toggle('bi-folder');
         this.parentElement.querySelector('.nested_in_folder').classList.toggle('active_folder');
     });
 }
