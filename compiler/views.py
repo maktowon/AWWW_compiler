cc07df96cf3782e17d0c6cf0827218bd86c4ff2f
aktowon <maciek.nowotek@gmail.com>
2023-05-07 16:10:27 +0200

    add basic register and login

--- compiler/views.py
+++ compiler/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.contrib import messages
 from .models import File, Directory
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
@@ -8,6 +13,36 @@ def home(request):
     root_files = File.objects.filter(parent=None)
 
     return render(request, 'compiler/main.html', {'root_folders': root_folders, 'root_files': root_files})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'compiler/register.html', {'form_register': form})
+
+
+def login_to(request):
+    message = ''
+    if request.method == 'POST':
+        user_name = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user_name = User.objects.get(username=user_name)
+        except ObjectDoesNotExist:
+            message = 'not a valid username'
+            return render(request, 'compiler/login.html', {'message': message})
+        user_authenticate = authenticate(request, username=user_name, password=password)
+        if user_authenticate is not None:
+            login(request, user_authenticate)
+            return redirect('home')
+        else:
+            message = 'wrong pasword'
+    return render(request, 'compiler/login.html', {'message': message})
 
 
 def show_code(request, pk):
